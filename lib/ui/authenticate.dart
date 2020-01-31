ccd2dfdfe40fdaafe63943d91d36604475164f37
Akash Tandon <akashtndn.acm@gmail.com>
2020-02-01 01:11:52 +0530

    adds screens for OTP verification and sign-in

--- lib/ui/authenticate.dart
+++ lib/ui/authenticate.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tassist/ui/views/register.dart';
 import 'package:tassist/ui/views/sign_in.dart';
+import 'package:tassist/ui/views/sign_in_phone.dart';
 
 class Authenticate extends StatefulWidget {
   @override
@@ -18,7 +19,7 @@ class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     if (showSignIn) {
-      return SignIn(toggleView:  toggleView);
+      return SignInPhone(toggleView:  toggleView);
     } else {
       return Register(toggleView:  toggleView);
     }
