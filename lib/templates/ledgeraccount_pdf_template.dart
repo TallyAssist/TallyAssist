bed18beb2c24f0fd2267edb025aa6f37959e4c93
RichaVerma <richa.verma8792@gmail.com>
2020-04-18 17:13:39 +0530

    updated party statement for UI improvements

--- lib/templates/ledgeraccount_pdf_template.dart
+++ lib/templates/ledgeraccount_pdf_template.dart
@@ -7,8 +7,8 @@ createLedgerPdf({
   String startDate,
   String endDate,
   List<List<String>> ledgerList,
-  // String openingBalance,
-  // String closingBalance,
+  String openingBalance,
+  String closingBalance,
   // String totalCreditWithoutClosingBalance,
   // String totalDebitWithoutClosingBalance,
   // String totalCredit,
@@ -24,15 +24,30 @@ createLedgerPdf({
         if (context.pageNumber == 1) {
           return Header(
             level: 0,
+            // margin: const EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
+          padding: const EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
+            
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(companyName, textScaleFactor: 1.5),
                 SizedBox(height: 10),
-                Text(partyName, textScaleFactor: 1.5),
-                Text('Ledger Account', textScaleFactor: 1.10),
-                SizedBox(height: 20),
-                Text('$startDate to $endDate', textScaleFactor: 1.10),
+                Text(partyName, textScaleFactor: 1.2),
+                Text('Ledger Account'),
+                SizedBox(height: 10),
+                Text('$startDate to $endDate'),
+                Container(
+                  padding: const EdgeInsets.only(top: 0.5 * PdfPageFormat.cm),
+                  child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Opening balance: $openingBalance'),
+                  Text('Closing balance: $closingBalance')
+                ],
+              )
+              ),
               ],
             ),
           );
@@ -49,35 +64,57 @@ createLedgerPdf({
               Container(
                 child: Text(companyName),
               ),
+              Container(
+                  padding: const EdgeInsets.only(top: 0.3 * PdfPageFormat.cm),
+                  child:  Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('$partyName'),
+                  Text('Ledger Account: $startDate to $endDate')
+                ],
+              ),
+
+              ),
+              Container(
+                  padding: const EdgeInsets.only(top: 0.3* PdfPageFormat.cm),
+                  child:
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text('$partyName   Ledger Account: $startDate to $endDate'),
-                  Text('Page ${context.pageNumber}'),
+                  Text('Opening balance: $openingBalance'),
+                  Text('Closing balance: $closingBalance')
                 ],
+              )
               ),
-              Table.fromTextArray(context: context, data: [
-                [
-                  'Date',
-                  'Particulars',
-                  'Vch Type',
-                  'Vch No.',
-                  'Debit',
-                  'Credit'
-                ],
-              ]),
+              // Table.fromTextArray(context: context, data: [
+              //   [
+              //     'Date',
+              //     'Particulars',
+              //     'Vch Type',
+              //     'Vch No.',
+              //     'Debit',
+              //     'Credit'
+              //   ],
+              // ]),
             ],
           ),
         );
       },
       footer: (Context context) {
         return Container(
-            alignment: Alignment.centerRight,
+            // alignment: Alignment.centerRight,
             margin: const EdgeInsets.only(top: 1.0 * PdfPageFormat.cm),
-            child: Text('Page ${context.pageNumber} of ${context.pagesCount}',
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget> [
+            Text('Generated using TallyAssist',  style: TextStyle(color: PdfColors.purple400, font: Font.timesItalic())),
+            Text('Page ${context.pageNumber} of ${context.pagesCount}',
                 style: Theme.of(context)
                     .defaultTextStyle
-                    .copyWith(color: PdfColors.grey)));
+                    .copyWith(color: PdfColors.grey))
+          ]
+        ),
+                    );
       },
       build: (Context context) => <Widget>[
         SizedBox(height: 30),
