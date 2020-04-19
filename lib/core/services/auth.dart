0f3fc5353330739afba738f353ed7362685e9b90
Akash Tandon <akashtndn.acm@gmail.com>
2020-04-19 22:52:54 +0530

    create dummy date on user signup

--- lib/core/services/auth.dart
+++ lib/core/services/auth.dart
@@ -39,10 +39,127 @@ class AuthService {
           email: email, password: password);
       FirebaseUser user = result.user;
 
+      // Firestore.instance
+      //     .collection('metrics')
+      //     .document(user.uid)
+      //     .setData({'total_sales': 0});
+
+      // Company
+      Firestore.instance.collection('company').document(user.uid).setData({
+        'email': user.email,
+      });
+
+      // Stock
       Firestore.instance
-          .collection('metrics')
+          .collection('company')
           .document(user.uid)
-          .setData({'total_sales': 0});
+          .collection('stockitem')
+          .document('1')
+          .setData({
+        'name': 'Gur',
+        'master_id': '1',
+        'closingbalance': 100,
+        'closingvalue': 25000,
+        'baseunits': 'kg',
+        'closingrate': 250,
+        'standardcost': 225,
+        'standardprice': 240,
+      });
+      Firestore.instance
+          .collection('company')
+          .document(user.uid)
+          .collection('stockitem')
+          .document('2')
+          .setData({
+        'name': 'Wheat',
+        'master_id': '2',
+        'closingbalance': 10,
+        'closingvalue': 2500,
+        'baseunits': 'kg',
+        'closingrate': 250,
+        'standardcost': 200,
+        'standardprice': 225,
+      });
+
+      // Ledger
+      Firestore.instance
+          .collection('company')
+          .document(user.uid)
+          .collection('ledger')
+          .document('1')
+          .setData({
+        'name': 'XYZ Ltd',
+        'master_id': '1',
+        'currencyname': 'Rupees',
+        'opening_balance': 0,
+        'closing_balance': 100000,
+        'parentcode': '',
+        'contact': '',
+        'address': 'CP, New Delhi',
+        'state': 'Delhi',
+        'pincode': '110001',
+        'email': 'xyz@email.com',
+        'phone': '9999999999',
+        'guid': '1',
+        'gst': 'XXXXXXXXXXXX',
+      });
+
+      // Voucher
+      Firestore.instance
+          .collection('company')
+          .document(user.uid)
+          .collection('voucher')
+          .document('1')
+          .setData({
+        'date': DateTime.now(),
+        'restat_party_ledger_name': 'XYZ Ltd',
+        'amount': 10000,
+        'master_id': '1',
+        'is_cancelled': '0',
+        'primary_voucher_type_name': 'Sales',
+        'is_invoice': '1',
+        'type': 'Sales',
+        'party_legger_name': '1',
+        'number': '1',
+        'inventory_entries': [
+          {
+            'voucher_date': DateTime.now(),
+            'voucher_master_id': '1',
+            'rate': 220,
+            'primary_voucher_type_name': 'Sales',
+            'gstpercent': '18',
+            'billedqty': 10,
+            'actualqty': 10,
+            'amount': 2596,
+            'taxamount': 396,
+          }
+        ],
+        'ledger_entries': [],
+      });
+
+      Firestore.instance
+          .collection('company')
+          .document(user.uid)
+          .collection('inventory_entries')
+          .document('1')
+          .setData({
+        'restat_stock_item_name': 'Wheat',
+        'restat_party_ledger_name': 'XYZ Ltd',
+        'voucher_date': DateTime.now(),
+        'voucher_master_id': '1',
+        'rate': 220,
+        'primary_voucher_type_name': 'Sales',
+        'gstpercent': '18',
+        'billedqty': 10,
+        'actualqty': 10,
+        'amount': 2596,
+        'taxamount': 396,
+      });
+
+      // Firestore.instance
+      //     .collection('company')
+      //     .document(user.uid)
+      //     .collection('ledger_entries');
 
       return _userFromFirebaseUser(user);
     } catch (error) {
